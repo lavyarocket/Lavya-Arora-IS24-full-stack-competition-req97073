ae0877dbab389d51982af61d1a45fe2e2f5bc1f3
Lavya <lavyarocket@gmail.com>
2023-03-26 01:46:53 -0700

    Refactor to jsx style components

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [searchedColumn, setSearchedColumn] = useState("");
+  const [visibleEntriesCount, setVisibleEntriesCount] = useState(0);
   const searchInput = useRef(null);
 
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
@@ -17,11 +18,11 @@ function App() {
     setSearchText(selectedKeys[0]);
     setSearchedColumn(dataIndex);
   };
-  
-  const handleReset = (selectedKeys, confirm, dataIndex) => {
-    confirm();
+
+  const handleReset = (clearFilters, confirm) => {
+    clearFilters();
     setSearchText("");
-    setSearchedColumn(dataIndex);
+    confirm();
   };
 
   function filterIcon(filtered) {
@@ -31,90 +32,125 @@ function App() {
           color: filtered ? "#1890ff" : undefined,
         }}
       />
-    )
+    );
   }
 
   function onFilter(value, record, dataIndex) {
     return record[dataIndex]
     .toString()
     .toLowerCase()
-    .includes(value.toLowerCase())
+    .includes(value.toLowerCase());
   }
 
-  function onFilterDropdownOpenChange: (visible) => {
+  function onFilterDropdownOpenChange(visible) {
     if (visible) {
       setTimeout(() => searchInput.current?.select(), 100);
     }
   }
 
-  function filterDropdown({
-    setSelectedKeys,
-    selectedKeys,
-    confirm,
-    clearFilters,
-    close,
-  }) { return (
-    <div
-      style={{
-        padding: 8,
-      }}
-      onKeyDown={(e) => e.stopPropagation()}
-    >
-      <Input
-        ref={searchInput}
-        placeholder={`Search ${placeholder}`}
-        value={selectedKeys[0]}
-        onChange={(e) => {
-          const value = e.target.value ? [e.target.value] : [];
-
-          setSelectedKeys(value);
+  function filterDropdown(
+    { setSelectedKeys, selectedKeys, confirm, clearFilters, close },
+    dataIndex,
+    placeholder
+  ) {
+    return (
+      <div
+        style={{
+          padding: 8,
         }}
-        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-        className="block mb-2"
-      />
-      <Space>
-        <Button
-          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          icon={<SearchOutlined />}
-          size="small"
-          className="flex items-center"
-          style={{
-            width: 90,
-          }}
-        >
-          Search
-        </Button>
-        <Button
-          onClick={() => clearFilters && handleReset(selectedKeys, confirm, dataIndex)}
-          size="small"
-          style={{
-            width: 90,
-          }}
-        >
-          Reset
-        </Button>
-      </Space>
-    </div>
-  )}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${placeholder}`}
+          value={selectedKeys[0]}
+          onChange={(e) => {
+            const value = e.target.value ? [e.target.value] : [];
 
-  function renderText(value, record, dataIndex) {
-    return value;
+            setSelectedKeys(value);
+          }}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          className="block mb-2"
+        />
+        <Space>
+          <Button
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            className="flex items-center"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() =>
+              clearFilters && handleReset(clearFilters, confirm)
+            }
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    );
+  }
+
+  function renderText(value, record, index, dataIndex) {
+    if (searchedColumn === dataIndex) {
+      return (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#FECE02",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={value ? value.toString() : ""}
+        />
+      );
+    } else {
+      return value;
+    }
   }
 
   function renderDate(date, record, dataIndex) {
     return date;
   }
 
-  function renderArray(values, record, dataIndex){
-    return (
-      <div className="flex flex-col"> 
-        {values.map((value, idx) => {
-        return (
-          <div key={idx}>{value}</div>
-        )
-      })}
-      </div>
-    )
+  function renderArray(values, record, index, dataIndex) {
+    if (searchedColumn === dataIndex) {
+      return (
+        <div className="flex flex-col">
+          {values.map((value, idx) => {
+            return (
+              <Highlighter
+                key={idx}
+                highlightStyle={{
+                  backgroundColor: "#FECE02",
+                  padding: 0,
+                }}
+                searchWords={[searchText]}
+                autoEscape
+                textToHighlight={value ? value.toString() : ""}
+              />
+            );
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col">
+          {values.map((value, idx) => {
+            return <div key={idx}>{value}</div>;
+          })}
+        </div>
+      );
+    }
   }
 
   function renderAction(value, record, dataIndex) {
@@ -123,7 +159,7 @@ function App() {
         <Button>Edit</Button>
         <Button>Delete</Button>
       </Space>
-    )
+    );
   }
 
   async function fetchProductData() {
@@ -131,7 +167,8 @@ function App() {
 
     try {
       const response = await axios.get("http://localhost:3000/api/products/");
-
+      
+      setVisibleEntriesCount(response.data.length);
       setData(response.data);
     } catch (err) {
       toast.err(err.response.data.message);
@@ -149,7 +186,7 @@ function App() {
       <main className="h-screen w-full p-8 flex items-center justify-center bg-[#F9FAFA]">
         <div className="h-full max-h-[70%] w-fit max-w-[90%] flex flex-col gap-4">
           <div className="flex items-center justify-between shrink-0">
-            <span>Total Entries: </span>
+            <span>Entries Displayed: {visibleEntriesCount}</span>
             <Button>Add</Button>
           </div>
           <div className="overflow-auto relative flex-1">
@@ -159,15 +196,77 @@ function App() {
               pagination={false}
               sticky={true}
               className="max-w-full"
+              onChange={(pagination, filters, sorter, { currentDataSource })=>{
+                setVisibleEntriesCount(currentDataSource.length);
+              }}
             >
-              <Table.Column width="12.5%" title="Product ID" dataIndex="productId" key="productId" render={renderText}  />
-              <Table.Column width="12.5%" title="Product Name" dataIndex="productName" key="productName" render={renderText}/>
-              <Table.Column width="12.5%" title="Product Owner" dataIndex="productOwnerName" key="productOwnerName" render={renderText}/>
-              <Table.Column width="12.5%" title="Developers" dataIndex="Developers" key="Developers" render={renderArray} />
-              <Table.Column width="12.5%" title="Scrum Master" dataIndex="scrumMasterName" key="scrumMasterName" render={renderText}/>
-              <Table.Column width="12.5%" title="Start Date" dataIndex="startDate" key="startDate" render={renderDate}/>
-              <Table.Column width="12.5%" title="Methodology" dataIndex="methodology" key="methodology" render={renderText} />
-              <Table.Column width="12.5%" title="Action" key="action" render={renderAction}/>
+              <Table.Column
+                width="12.5%"
+                title="Product ID"
+                dataIndex="productId"
+                key="productId"
+                render={renderText}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Product Name"
+                dataIndex="productName"
+                key="productName"
+                render={renderText}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Product Owner"
+                dataIndex="productOwnerName"
+                key="productOwnerName"
+                render={renderText}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Developers"
+                dataIndex="Developers"
+                key="Developers"
+                render={(...args)=>renderArray(...args, "Developers")}
+                onFilter={(...args)=>onFilter(...args, "Developers")}
+                filterDropdown={(props) =>
+                  filterDropdown(props, "Developers", "Developer")
+                }
+                filterIcon={filterIcon}
+                onFilterDropdownOpenChange={onFilterDropdownOpenChange}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Scrum Master"
+                dataIndex="scrumMasterName"
+                key="scrumMasterName"
+                render={(...args)=>renderText(...args, "scrumMasterName")}
+                onFilter={(...args)=>onFilter(...args, "scrumMasterName")}
+                filterDropdown={(props) =>
+                  filterDropdown(props, "scrumMasterName", "Scrum Master")
+                }
+                filterIcon={filterIcon}
+                onFilterDropdownOpenChange={onFilterDropdownOpenChange}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Start Date"
+                dataIndex="startDate"
+                key="startDate"
+                render={renderDate}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Methodology"
+                dataIndex="methodology"
+                key="methodology"
+                render={renderText}
+              />
+              <Table.Column
+                width="12.5%"
+                title="Action"
+                key="action"
+                render={renderAction}
+              />
             </Table>
           </div>
         </div>
